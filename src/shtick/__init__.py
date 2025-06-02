29658f63572679700d47eb74274ac9ec69e3aa40
espadonne <espadonne@outlook.com>
2025-06-02 15:33:14 -0400

    api

--- src/shtick/__init__.py
+++ src/shtick/__init__.py
@@ -3,3 +3,9 @@ shtick - Shell alias manager
 """
 
 __version__ = "1.0.0"
+
+# Export the high-level API for easy importing
+from .shtick import ShtickManager
+
+# Make it available at package level
+__all__ = ["ShtickManager"]
